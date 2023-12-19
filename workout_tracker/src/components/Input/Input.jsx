c3f07c3d9ae273895dc4d4dc1cd791f47dc1ae84
Jason Guan <Jason.Guan21@thehutgroup.com>
2023-12-19 16:18:45 +0000

    Make row highlight

--- workout_tracker/src/components/Input/Input.jsx
+++ workout_tracker/src/components/Input/Input.jsx
@@ -1,13 +1,22 @@
 import "./Input.css";
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 export const Input = ({ setNumber }) => {
+  const [backgroundColor, setBackgroundColor] = useState("transparent");
+  const handleCheck = () => {
+    const newBackgroundColor =
+      backgroundColor === "transparent" ? "#84e88e" : "transparent";
+    setBackgroundColor(() => newBackgroundColor);
+  };
   return (
-    <div className="inputRow">
+    <div className="inputRow" style={{ backgroundColor: `${backgroundColor}` }}>
       <span className="setNumber">{setNumber}</span>
       <input type="text" className="weight" placeholder="kg" />
       <input type="text" className="reps" placeholder="reps" />
-      <button className="complete">{"\u2713"}</button>
+      <button className="complete" onClick={handleCheck}>
+        {"\u2713"}
+      </button>
     </div>
   );
 };
